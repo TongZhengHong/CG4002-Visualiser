abe985dd0167022bafd0a5d0219e615085f213b2
TongZhengHong <zhtong315@gmail.com>
2025-03-15 13:51:45 +0800

    Remove rigidbody on projectiles, use biezer curves instead

--- Assets/Scripts/ActionProjectile.cs
+++ Assets/Scripts/ActionProjectile.cs
@@ -21,12 +21,6 @@ public class ActionProjectile: MonoBehaviour
 
     [SerializeField] private float duration = 3f;
 
-    [SerializeField] private float launchForce = 3f;
-
-    [SerializeField] private float gravityReduction = 3f;
-
-    [SerializeField] private float heightOffset = 0.5f;
-
     // [SerializeField] private bool useFixedDistance = false;
 
     [Header("Animation Options")]
@@ -43,8 +37,6 @@ public class ActionProjectile: MonoBehaviour
 
     private Vector3 turningPoint = Vector3.zero;
 
-    private Rigidbody rigidBody;
-
     public Vector3 Evaluate(float t) 
     {
         if (start == null || end == null || turningPoint == null) return Vector3.zero;
@@ -56,41 +48,16 @@ public class ActionProjectile: MonoBehaviour
 
     public void OnLaunchProjectile()
     {
-        // Collider collider = GetComponent<Collider>();
-        // Collider camCollider = Camera.main.GetComponent<Collider>();
-        // Physics.IgnoreCollision(collider, camCollider);
+        start = transform.position;
+        start.y -= startingHeightOffset; // Offset below eye/camera level
+        start += transform.right * 0.2f; // Start projectile right of center 
+        end = start + transform.forward * totalDistance;
 
-        rigidBody = GetComponent<Rigidbody>();
-        Vector3 launchDirection = transform.forward; 
-        launchDirection.y += heightOffset;
+        Vector3 midPoint = (start + end) / 2;
+        turningPoint = new Vector3(midPoint.x, midPoint.y + projectileHeight, midPoint.z);
 
-        rigidBody.AddForce(launchDirection.normalized * launchForce, ForceMode.Impulse);
-        rigidBody.AddForce(Vector3.up * gravityReduction);
-        // start = transform.position;
-        // start.y -= startingHeightOffset; // Offset below eye/camera level
-        // start += transform.right * 0.2f; // Start projectile right of center 
-        // end = start + transform.forward * totalDistance;
-
-        // Vector3 midPoint = (start + end) / 2;
-        // turningPoint = new Vector3(midPoint.x, midPoint.y + projectileHeight, midPoint.z);
-
-        // StopAllCoroutines();
-        // StartCoroutine(AnimateProjectile());
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.name == "GroundReferencePlane")
-        {
-            // if (snowPrefab != null) //&& ReticlePointer.isLookingAtQR 
-            // {
-            //     Vector3 snowPos = transform.position;
-            //     GameObject snowObject = Instantiate(snowPrefab, snowPos, Quaternion.Euler(Vector3.up));
-            //     ParticleSystem snowParticles = snowObject.GetComponentInChildren<ParticleSystem>();
-            //     snowParticles.Play();
-            // }
-            Destroy(gameObject); // Destroy projectile on impact
-        }
+        StopAllCoroutines();
+        StartCoroutine(AnimateProjectile());
     }
 
     public IEnumerator AnimateProjectile()
@@ -119,14 +86,6 @@ public class ActionProjectile: MonoBehaviour
             elapsed += Time.deltaTime;
             yield return null;
         }
-
-        // if (snowPrefab != null) //&& ReticlePointer.isLookingAtQR 
-        // {
-        //     Vector3 snowPos = transform.position;
-        //     GameObject snowObject = Instantiate(snowPrefab, snowPos, Quaternion.Euler(Vector3.up));
-        //     ParticleSystem snowParticles = snowObject.GetComponentInChildren<ParticleSystem>();
-        //     snowParticles.Play();
-        // }
 
         GameObject explosionObject = Instantiate(explosionPrefab, transform.position, Quaternion.Euler(Vector3.up));
         ParticleSystem[] explosionParticles = explosionObject.GetComponentsInChildren<ParticleSystem>();
